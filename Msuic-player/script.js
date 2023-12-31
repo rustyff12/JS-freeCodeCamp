2e657a58a8bc25480377a9fe91f128805167e36f
rustyff12 <russellfrankpitt@googlemail.com>
2023-12-31 12:49:07 +0100

    initial commit. index.html, styles.css, script.js added

---
+++ Msuic-player/script.js
@@ -0,0 +1,27 @@
+// Music Player
+
+// Assign variables to respective id's
+const playlistSongs = document.getElementById('playlist-songs');
+const playButton = document.getElementById('play');
+const pauseButton = document.getElementById('pause');
+const nextButton = document.getElementById('next');
+const previousButton = document.getElementById('previous');
+const shuffleButton = document.getElementById('shuffle');
+
+// Create an array to score all the songs
+const allSongs = [
+    {
+        id: 0,
+        title: "Scratching The Surface",
+        artist: "Quincy Larson",
+        duration: "4:25",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/scratching-the-surface.mp3",
+    },
+    {
+        id: 1,
+        title: "Can't Stay Down",
+        artist: "Quincy Larson",
+        duration: "4:15",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stay-down.mp3",
+    }
+];
